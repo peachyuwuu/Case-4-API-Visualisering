f51a8193142f69d9b09cd8f9a203ac07b84d56c4
peachyuwuu <jonte55@live.se>
2023-02-07 14:58:26 +0100

    added ability to search the NASA image and video library

--- script.js
+++ script.js
@@ -15,16 +15,36 @@
 // Hämta bild från API:et
 
 
+// Lägg till &count=10 i slutet för att få en array med 10 slumpmässiga objekt
 
 // fetch("https://api.nasa.gov/planetary/apod?api_key=GrZaNTnebyHg8Y2vY7CBaAtgYyJviVsdVKMNt5zX&count=10")
 
-fetch("https://api.nasa.gov/planetary/apod?api_key=GrZaNTnebyHg8Y2vY7CBaAtgYyJviVsdVKMNt5zX&count=10")
-    .then(response => response.json())
-    .then((data) => {
 
-        console.log(data);
 
-        document.body.innerHTML = `<img src="${data[0].url}"><input type="text" placeholder="Type here"><button>Search</button>`
+const searchButton = document.querySelector("button");
 
-    });
-
+searchButton.addEventListener("click", function () {
+    // e.preventDefault()
+    
+    let search = document.querySelector("input");
+    
+    console.log("test");
+    
+    fetch(`https://images-api.nasa.gov/search?q=${search.value}`)
+        .then(response => response.json())
+        .then((data) => {
+    
+            console.log(data);
+    
+            // document.body.innerHTML = `<img src="${data.url}"><input type="text" placeholder="Type here"><button>Search</button>`
+    
+            for (let i = 0; i < data.length; i++) {
+                const element = data[i];
+                
+                document.body.innerHTML += `<img src="${element.href}">`
+    
+                console.log(element);
+            }
+        });
+    
+});
